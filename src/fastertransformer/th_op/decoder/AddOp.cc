edd78612d93724823705fd1776a978922cd3d45c
CjhHa1 <cjh18671720497@outlook.com>
2023-09-06 03:05:30 +0000

    change link

--- src/fastertransformer/th_op/decoder/AddOp.cc
+++ src/fastertransformer/th_op/decoder/AddOp.cc
@@ -1,7 +1,6 @@
 #include "src/fastertransformer/th_op/decoder/AddOp.h"
 
 namespace th = torch;
-
 namespace torch_ext {
 th::Tensor
 add(th::Tensor input, th::Tensor bias)
@@ -12,9 +11,14 @@ CHECK_TH_CUDA(bias);
 CHECK_CONTIGUOUS(bias);
 auto output = th::empty_like(input);
 
+    
 fastertransformer::invokeAdd(torch_ext::get_ptr<float>(input),
                               torch_ext::get_ptr<float>(bias),
                               torch_ext::get_ptr<float>(output));
+
+return output;
 }
 
 }  // namespace torch_ext
+
+static auto add = th::RegisterOperators("fastertransformer::add", &torch_ext::add);
